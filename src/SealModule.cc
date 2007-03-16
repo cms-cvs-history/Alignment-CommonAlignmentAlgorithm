4905d96757279c30000258e98ff6a031ac0342c8
cmsbuild <>
2007-03-16 16:37:53 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- CommonAlignmentAlgorithm-V01-03-00'.
    Sprout from master 2007-03-16 16:37:52 UTC Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch> 'LogInfo if failing propagation, cloningrechit only if improving (+temporary backport of header location)'
    Cherrypick from master 2006-10-31 17:44:07 UTC ewidl <sha1-3c65bb8ad5ab20d6db077c9cd74c65f6de0120f6@cern.ch> 'moved trajectory factory stuff to CommonAlignmentAlgorithm':
        src/SealModule.cc

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -2,7 +2,7 @@
 #include "Alignment/CommonAlignmentAlgorithm/interface/TrajectoryFactoryPlugin.h"
 #include "Alignment/CommonAlignmentAlgorithm/interface/ReferenceTrajectoryFactory.h"
 
-#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "PluginManager/ModuleDef.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_SEAL_PLUGIN( TrajectoryFactoryPlugin, ReferenceTrajectoryFactory, "ReferenceTrajectoryFactory" );
