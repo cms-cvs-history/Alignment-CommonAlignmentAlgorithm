eaadad6b8a30d3ad367aaf2a763fda845ee3e110
Joerg Behr <sha1-6fc71cbfef80ca4fef4a27bd037b04a2b7c5d6f8@cern.ch>
2013-04-23 08:13:27 +0000

    make LA selection granularity in pixel detector configureable

--- plugins/SiPixelLorentzAngleCalibration.cc
+++ plugins/SiPixelLorentzAngleCalibration.cc
@@ -7,8 +7,8 @@
 ///
 ///  \author    : Gero Flucke
 ///  date       : September 2012
-///  $Revision: 1.4.2.3 $
-///  $Date: 2013/04/17 09:37:31 $
+///  $Revision: 1.4.2.4 $
+///  $Date: 2013/04/22 08:26:47 $
 ///  (last update by $Author: flucke $)
 
 #include "Alignment/CommonAlignmentAlgorithm/interface/IntegratedCalibrationBase.h"
@@ -24,8 +24,12 @@
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ESWatcher.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "Alignment/CommonAlignment/interface/Alignable.h"
+
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "Alignment/CommonAlignmentAlgorithm/interface/AlignmentParameterSelector.h"
 
 #include "Geometry/CommonDetUnit/interface/GeomDet.h"
 #include "MagneticField/Engine/interface/MagneticField.h"
@@ -39,6 +43,10 @@
 #include <vector>
 #include <map>
 #include <sstream>
+#include <list>
+#include <utility>
+#include <set>
+
 #include <functional>
 
 class SiPixelLorentzAngleCalibration : public IntegratedCalibrationBase
@@ -86,9 +94,11 @@ public:
   virtual double getParameterError(unsigned int index) const;
 
   // /// Call at beginning of job:
-  // virtual void beginOfJob(const AlignableTracker *tracker,
-  // 			  const AlignableMuon *muon,
-  // 			  const AlignableExtras *extras);
+  virtual void beginOfJob(AlignableTracker *tracker,
+  			  AlignableMuon *muon,
+  			  AlignableExtras *extras);
+
+
 
   /// Called at end of a the job of the AlignmentProducer.
   /// Write out determined parameters.
@@ -109,6 +119,9 @@ private:
   /// Index of parameter for given detId (detId not treated => < 0)
   /// and the given run.
   int getParameterIndexFromDetId(unsigned int detId, edm::RunNumber_t run) const;
+  int getParameterIndexFromDetId_old(unsigned int detId, edm::RunNumber_t run) const;
+
+
   /// Total number of IOVs.
   unsigned int numIovs() const;
   /// First run of iov (0 if iovNum not treated).
@@ -123,12 +136,22 @@ private:
   const std::string outFileName_;
   const std::vector<std::string> mergeFileNames_;
 
+  
+  const edm::VParameterSet myLAGranularityConfig_;
+  
   edm::ESWatcher<SiPixelLorentzAngleRcd> watchLorentzAngleRcd_;
 
   // const AlignableTracker *alignableTracker_;
   SiPixelLorentzAngle *siPixelLorentzAngleInput_;
   std::vector<double> parameters_;
   std::vector<double> paramUncertainties_;
+  
+  std::set<edm::RunNumber_t> globalRunRange_;
+  std::vector<unsigned int> firstId_;//1:1 mapping with LAassignment
+  std::vector<std::pair<std::list<Alignable*>, std::vector<edm::RunNumber_t> > > LAassignment_;
+
+ 
+ 
 };
 
 //======================================================================
@@ -141,6 +164,7 @@ SiPixelLorentzAngleCalibration::SiPixelLorentzAngleCalibration(const edm::Parame
     recordNameDBwrite_(cfg.getParameter<std::string>("recordNameDBwrite")),
     outFileName_(cfg.getParameter<std::string>("treeFile")),
     mergeFileNames_(cfg.getParameter<std::vector<std::string> >("mergeTreeFiles")),
+    myLAGranularityConfig_(cfg.getParameter<edm::VParameterSet>("LorentzAngleGranularity")),
     //    alignableTracker_(0),
     siPixelLorentzAngleInput_(0)
 {
@@ -151,8 +175,8 @@ SiPixelLorentzAngleCalibration::SiPixelLorentzAngleCalibration(const edm::Parame
 //  paramUncertainties_.resize(349, 0.); // dito for errors
 //  parameters_.resize(1072, 0.); // currently two parameters BPIX:6, FPIX:2, 134 IOVs, start value 0.
 //  paramUncertainties_.resize(1072, 0.); // dito for errors
-  parameters_.resize(1274, 0.); // BPIX:24, FPIX:2, 49 IOVs, start value 0.
-  paramUncertainties_.resize(1274, 0.); // dito for errors
+  // parameters_.resize(1274, 0.); // BPIX:24, FPIX:2, 49 IOVs, start value 0.
+  // paramUncertainties_.resize(1274, 0.); // dito for errors
 
   edm::LogInfo("Alignment") << "@SUB=SiPixelLorentzAngleCalibration" << "Created with name "
                             << this->name() << "',\n" << this->numParameters() << " parameters to be determined,"
@@ -234,7 +258,7 @@ bool SiPixelLorentzAngleCalibration::setParameter(unsigned int index, double val
   if (index >= parameters_.size()) {
     return false;
   } else {
-    parameters_[index] = value;
+    parameters_.at(index) = value;
     return true;
   }
 }
@@ -245,7 +269,7 @@ bool SiPixelLorentzAngleCalibration::setParameterError(unsigned int index, doubl
   if (index >= paramUncertainties_.size()) {
     return false;
   } else {
-    paramUncertainties_[index] = error;
+    paramUncertainties_.at(index) = error;
     return true;
   }
 }
@@ -258,7 +282,7 @@ double SiPixelLorentzAngleCalibration::getParameter(unsigned int index) const
   //   } else {
   //     return parameters_[index];
   //   }
-  return (index >= parameters_.size() ? 0. : parameters_[index]);
+  return (index >= parameters_.size() ? 0. : parameters_.at(index));
 }
 
 //======================================================================
@@ -269,16 +293,134 @@ double SiPixelLorentzAngleCalibration::getParameterError(unsigned int index) con
   //   } else {
   //     return paramUncertainties_[index];
   //   }
-  return (index >= paramUncertainties_.size() ? 0. : paramUncertainties_[index]);
+  return (index >= paramUncertainties_.size() ? 0. : paramUncertainties_.at(index));
 }
 
-// //======================================================================
-// void SiPixelLorentzAngleCalibration::beginOfJob(const AlignableTracker *tracker,
-//                                                 const AlignableMuon */*muon*/,
-//                                                 const AlignableExtras */*extras*/)
-// {
-//   alignableTracker_ = tracker;
-// }
+
+
+
+//======================================================================
+void SiPixelLorentzAngleCalibration::beginOfJob(AlignableTracker *aliTracker,
+                                                AlignableMuon *aliMuon,
+                                                AlignableExtras *aliExtras)
+{
+  unsigned int nparameters = 0;
+  unsigned int Id = 0;
+  //loop over all LA groups
+  for(edm::VParameterSet::const_iterator pset = myLAGranularityConfig_.begin();
+      pset != myLAGranularityConfig_.end();
+      ++pset) {
+    const std::vector<edm::RunNumber_t> range = pset->getParameter<std::vector<edm::RunNumber_t> >("RunRange");
+
+    bool split = false;
+    if((*pset).exists("split")) {
+      split = pset->getParameter<bool>("split");
+    }
+    const size_t npar = (*pset).getParameterNames().size();
+    
+    if (npar >= 3 && !(*pset).exists("split")) {
+      throw cms::Exception("BadConfig")
+        << " >= 3 parameters specified in PSet BUT split parameter was not found! Maybe a typo?";
+    }
+    
+    AlignmentParameterSelector selector(aliTracker,aliMuon, aliExtras);
+    selector.clear();
+    selector.addSelections((*pset).getParameter<edm::ParameterSet> ("levels"));
+
+    const std::vector<Alignable*> &alis = selector.selectedAlignables();
+    
+
+    std::list<Alignable*> selected_alis;
+    for(std::vector<Alignable*>::const_iterator it = alis.begin(); it != alis.end(); it++) {
+      if((*it)->alignableObjectId() == align::AlignableDetUnit || (*it)->alignableObjectId() == align::AlignableDet) {
+        if(split) {
+          LAassignment_.push_back(std::make_pair(std::list<Alignable*>(1,(*it)), range));
+          firstId_.push_back(Id);
+          Id += range.size();
+          nparameters += range.size();
+        } else {
+          selected_alis.push_back((*it)); //throw out HLS?
+        }
+      }
+      const std::vector<Alignable*> &aliDaughts = (*it)->deepComponents();
+      if(aliDaughts.size() > 0) {
+        for (std::vector<Alignable*>::const_iterator iD = aliDaughts.begin();
+             iD != aliDaughts.end(); ++iD) {
+          
+          if((*iD)->alignableObjectId() == align::AlignableDetUnit || (*iD)->alignableObjectId() == align::AlignableDet) {
+            bool found = false;
+            for(unsigned int iAlignableGroup = 0; iAlignableGroup < firstId_.size(); iAlignableGroup++) {
+              const std::list<Alignable*> &a = LAassignment_.at(iAlignableGroup).first;
+              
+              for(std::list<Alignable*>::const_iterator iAli = a.begin();
+                  iAli != a.end(); iAli++) {
+                if( (*iAli)->alignableObjectId() == (*iD)->alignableObjectId()
+                    && (*iAli)->id() == (*iD)->id()) {
+                  found = true;
+                  if(!split) {
+                    throw cms::Exception("BadConfig")
+                      << " Alignable already selected.";
+                  }
+                  break;
+                }
+              }
+            }
+            if(split) {
+              if(!found) {
+                LAassignment_.push_back(std::make_pair(std::list<Alignable*>(1,(*iD)), range));
+                firstId_.push_back(Id);
+                Id += range.size();
+                nparameters += range.size();
+              }
+            } else {
+              selected_alis.push_back((*iD));
+            }
+            
+          
+
+
+
+          }
+        }
+      }
+
+    }
+ 
+   
+    //FIXME: add some checks whether the content of range makes sense?
+
+
+
+  
+    if(!split) {
+      LAassignment_.push_back(std::make_pair(selected_alis, range));
+      firstId_.push_back(Id);
+      Id += range.size();
+      nparameters += range.size();
+    }
+    
+    edm::RunNumber_t firstRun = 0; 
+    for(std::vector<edm::RunNumber_t>::const_iterator iRun = range.begin(); 
+        iRun != range.end(); ++iRun)  {
+      globalRunRange_.insert((*iRun));
+      if((*iRun) > firstRun) {
+        firstRun = (*iRun);
+      } else {
+        throw cms::Exception("BadConfig")
+          << "@SUB=SiPixelLorentzAngleCalibration::beginOfJob:"
+          << " Run range not sorted.";
+      }
+    }
+  }
+  
+
+  parameters_.resize(nparameters, 0.);
+  paramUncertainties_.resize(nparameters, 0.);
+  
+ 
+
+ 
+}
 
 
 //======================================================================
@@ -430,13 +572,85 @@ double SiPixelLorentzAngleCalibration::getParameterForDetId(unsigned int detId,
 //      << " Run: " << run
 //      << " Index: " << index;
 //  }
-  return (index < 0 ? 0. : parameters_[index]);
+  return (index < 0 ? 0. : parameters_.at(index));
 }
 
 //======================================================================
 int SiPixelLorentzAngleCalibration::getParameterIndexFromDetId(unsigned int detId,
 							       edm::RunNumber_t run) const
 {
+  // Return the index of the parameter that is used for this DetId.
+  // If this DetId is not treated, return values < 0.
+  
+  const PXBDetId temp_id(detId);
+  // const unsigned int nLayers=3;
+  // const unsigned int nRings=8;
+  int index = -1;
+  if (temp_id.det() != DetId::Tracker || ( temp_id.subdetId() != PixelSubdetector::PixelBarrel && temp_id.subdetId() != PixelSubdetector::PixelEndcap)) return -1;
+  
+
+  int nmatched = 0;
+  bool matched = false;
+  for(unsigned int iAlignableGroup = 0; iAlignableGroup < firstId_.size(); iAlignableGroup++) {
+    const unsigned int id0 = firstId_.at(iAlignableGroup);
+    
+    const std::list<Alignable*> &alis = LAassignment_.at(iAlignableGroup).first;
+    const std::vector<edm::RunNumber_t> &runs = LAassignment_.at(iAlignableGroup).second;
+    
+  
+    for(std::list<Alignable*>::const_iterator iAli = alis.begin();
+        iAli != alis.end(); iAli++) {
+      if( ((*iAli)->alignableObjectId() == align::AlignableDetUnit
+           || (*iAli)->alignableObjectId() == align::AlignableDet)
+          && (*iAli)->id() == detId) {
+        nmatched++;
+        break;
+      }
+    }
+    if(!matched && nmatched == 1) {
+      matched = true;
+      int iovNum = -1;
+      for(std::vector<edm::RunNumber_t>::const_iterator itRun = runs.begin();
+          itRun != runs.end(); itRun++) {
+        const edm::RunNumber_t r = (*itRun);
+        if(run >= r) iovNum++;
+      }
+
+      if(iovNum == -1) {
+        throw cms::Exception("BadRunRange")
+	  << "@SUB=SiPixelLorentzAngleCalibration::getParameterIndexFromDetId:\n"
+	  << "Bad run range for detid ('"<< detId <<"').";
+      }
+      index = id0 + iovNum;
+      // const PXBDetId temp_id(detId);
+      // const unsigned int nLayers=3;
+      // const unsigned int nRings=8;
+      // int index_old = -1;
+      // if(temp_id.subdetId() == PixelSubdetector::PixelBarrel) {
+      //   const PXBDetId id(detId);
+      //   index_old = iovNum*(nLayers*nRings+2)+(id.layer()-1)*(nRings)+(id.module()-1);
+      // } else if(temp_id.subdetId() == PixelSubdetector::PixelEndcap) { 
+      //   const PXFDetId id(detId);
+      //   index_old = iovNum*(nLayers*nRings+2)+nLayers*nRings+(id.side()-1);
+      // }
+    }
+  }
+
+  if(nmatched >= 2) {
+    throw cms::Exception("BadConfig")
+      << "@SUB=SiPixelLorentzAngleCalibration::getParameterIndexFromDetId:\n"
+      << "Detid ('"<< detId <<"') is assigned to >= 2 groups of modules (in fact it is matched to '"<< nmatched <<"' groups).";
+  }
+  
+
+  return index;
+}
+
+//======================================================================
+int SiPixelLorentzAngleCalibration::getParameterIndexFromDetId_old(unsigned int detId,
+                                                                   edm::RunNumber_t run) const
+{
+  //int index = -1;
   // Return the index of the parameter that is used for this DetId.
   // If this DetId is not treated, return values < 0.
   
@@ -504,7 +718,7 @@ unsigned int SiPixelLorentzAngleCalibration::numIovs() const
 //  return 58; 
 //  return 134;
 //  return 119;	  // 100 pb-1
-  return 49;	// 300 pb-1
+  return globalRunRange_.size();
 }
 
 //======================================================================
@@ -517,9 +731,24 @@ edm::RunNumber_t SiPixelLorentzAngleCalibration::firstRunOfIOV(unsigned int iovN
 //  unsigned int runNumbers[58] = {1,190707,190895,191086,191226,191271,191700,191811,191845,193336,193621,194050,194108,194120,194199,194225,194315,194428,194455,194480,194627,194691,194711,194789,194912,195016,195113,195163,195266,195304,195378,195397,195399,195540,195552,195655,195757,195774,195915,195937,195950,196096,196197,196218,196250,196353,196364,196433,196438,196452,196453,196531,198212,198230,198271,198487,198955,198969};    // 58 IOVs
 //  unsigned int runNumbers[134] = {1,190707,190895,191086,191226,191271,191700,191811,191845,193336,193621,194050,194108,194120,194199,194225,194315,194428,194455,194480,194627,194691,194711,194789,194912,195016,195113,195163,195266,195304,195378,195397,195399,195540,195552,195655,195757,195774,195915,195937,195950,196096,196197,196218,196250,196353,196364,196433,196438,196452,196453,196531,198212,198230,198271,198487,198955,198969,199021,199319,199409,199435,199569,199608,199703,199754,199812,199833,199864,199876,199960,200041,200075,200091,200190,200244,200369,200473,200519,200525,200600,200786,200991,201097,201168,201191,201278,201410,201424,201602,201624,201625,201671,201707,201802,201824,202016,202045,202060,202084,202087,202178,202237,202272,202304,202328,202472,202478,202504,202973,203002,203894,203912,203987,204113,204250,204541,204553,204563,204564,204577,204601,205158,205215,205238,205311,205344,205595,205620,205667,205694,205774,205826,205921};  // 119 IOVs
 //  unsigned int runNumbers[119] = {1,190738,191056,191226,191271,191721,191834,193336,193621,194050,194108,194150,194210,194314,194424,194429,194479,194533,194691,194711,194789,194912,195016,195113,195163,195266,195304,195378,195397,195399,195540,195552,195655,195758,195774,195915,195947,195950,196197,196218,196250,196362,196364,196437,196452,196453,198063,198230,198271,198487,198955,198969,199021,199319,199409,199435,199571,199608,199703,199754,199812,199833,199864,199876,199960,200041,200075,200091,200190,200244,200381,200473,200519,200532,200600,200991,201097,201168,201191,201278,201613,201657,201671,201707,201802,202013,202016,202045,202060,202084,202087,202178,202237,202272,202314,202389,202478,202504,202973,203002,203894,203912,203987,204113,204250,204554,204564,204577,204601,205158,205217,205238,205311,205344,205617,205667,205718,205826,205921}; // 119 IOVs
-  unsigned int runNumbers[49] = {1,191226,191834,193998,194115,194315,194480,194711,194912,195147,195304,195398,195645,195775,195948,196203,196349,196438,196531,198272,198955,199021,199409,199569,199699,199832,199876,200049,200190,200473,200600,201164,201196,201611,201671,201817,202054,202088,202272,202477,202972,203894,204101,204554,204577,205193,205339,205666,205781}; // 49 IOVs
-  if (iovNum < this->numIovs()) return runNumbers[iovNum];
-  else return 0;
+  // unsigned int runNumbers[49] = {1,191226,191834,193998,194115,194315,194480,194711,194912,195147,195304,195398,195645,195775,195948,196203,196349,196438,196531,198272,198955,199021,199409,199569,199699,199832,199876,200049,200190,200473,200600,201164,201196,201611,201671,201817,202054,202088,202272,202477,202972,203894,204101,204554,204577,205193,205339,205666,205781}; // 49 IOVs
+  // if (iovNum < this->numIovs()) return runNumbers[iovNum];
+  // else return 0;
+
+  
+
+  edm::RunNumber_t r = 0;
+
+  for(std::set<edm::RunNumber_t>::const_iterator itRun = globalRunRange_.begin();
+      itRun != globalRunRange_.end(); itRun++) {
+
+    if(std::distance(globalRunRange_.begin(), itRun ) == iovNum) {
+      r = (*itRun);
+          
+    }
+  }
+      
+  return r;
 
 }
 
